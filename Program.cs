1016fa43c6adee2eb1b326191a380f34ad21156c
usernamenahaichuk <gely231@mail.com>
2022-08-30 14:08:50 +0300

    30/08

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿int x = 5;
+int y = 2;
+int sum = 0;
+
+String result = "0";
+
+for (int i = Math.Min(x, y); i <= Math.Max(x, y); i++)
+{
+    sum = sum + i;
+    result = result + " + " + i.ToString();
+}
+Console.WriteLine(result + " = " + sum.ToString());
