677daf3eebc0932d119d5b3312d1d2827bbea012
usernamenahaichuk <gely231@mail.com>
2022-08-30 14:27:02 +0300

    30/08v2

--- Program.cs
+++ Program.cs
@@ -10,3 +10,25 @@ for (int i = Math.Min(x, y); i <= Math.Max(x, y); i++)
     result = result + " + " + i.ToString();
 }
 Console.WriteLine(result + " = " + sum.ToString());
+
+
+Console.Write("Введите число x: ");
+string? x1 = Console.ReadLine();
+
+if (x1 == "")
+{   
+    Console.Clear();
+}
+
+Console.Write("Введите число y: ");
+string? y1 = Console.ReadLine();
+if (y1 == "")
+{
+    Console.Clear();
+}
+
+if (y1 != "" & x1 != "")
+{
+    Console.WriteLine($"Установлено значение X:{x1} а Y: {y1}");
+}
+
